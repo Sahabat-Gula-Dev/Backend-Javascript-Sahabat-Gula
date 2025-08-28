e04dd5ec471293ba9bd70fc25b4904011a63f9e2
mhmadilham18 <f1d022061@student.unram.ac.id>
2025-08-29 06:41:52 +0800

    update logic auth strategy for access some endpoint

--- src/server.js
+++ src/server.js
@@ -2,12 +2,14 @@
 
 import Hapi from "@hapi/hapi";
 import dotenv from "dotenv";
-import auth from "./api/auth/index.js";
-import AuthService from "./services/supabase/AuthService.js";
-import TokenManager from "./tokenize/TokenManager.js";
 import HapiAuthJwt2 from "hapi-auth-jwt2";
 import AuthenticationError from "./exceptions/AuthenticationError.js";
-import InvariantError from "./exceptions/InvariantError.js";
+
+// for auth
+import auth from "./api/auth/index.js";
+import AuthService from "./services/supabase/AuthService.js";
+import AuthValidator from "./validator/auth/index.js";
+import TokenManager from "./tokenize/TokenManager.js";
 
 dotenv.config();
 
@@ -22,36 +24,39 @@ const init = async () => {
     },
   });
 
+  const authService = new AuthService();
+
   await server.register(HapiAuthJwt2);
 
   server.auth.strategy("jwt", "jwt", {
     keys: process.env.JWT_SECRET,
-    validate: (artifacts, request, h) => {
-      try {
-        const payload = TokenManager.verifyAccessToken(artifacts.token);
-        const isUserAdmin = payload.role === "admin";
-        if (request.route.path.startsWith("/admin") && !isUserAdmin) {
-          throw new AuthenticationError(
-            "You are not authorized to access this resource"
-          );
-        }
-        return { isValid: true, credentials: payload };
-      } catch (error) {
-        if (error instanceof AuthenticationError) {
-          return { isValid: false, error: error.message };
-        }
-        return { isValid: false, error: "Invalid token" };
+    validate: async (decoded, request, h) => {
+      if (request.route.path.startsWith("/admin") && decoded.role !== "admin") {
+        return {
+          isValid: false,
+        };
       }
+
+      const user = await authService._getUserProfile(decoded.id);
+      if (!user || !user.is_active) {
+        return {
+          isValid: false,
+        };
+      }
+
+      return {
+        isValid: true,
+        credentials: decoded,
+      };
     },
   });
-
-  const authService = new AuthService();
 
   await server.register({
     plugin: auth,
     options: {
       service: authService,
       tokenManager: TokenManager,
+      validator: AuthValidator,
     },
   });
 
